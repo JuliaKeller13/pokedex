e5369ac8cdd94738046b6a3fb4be91cc1af969f8
Julia Keller <juliakeller.vk@gmail.com>
2026-02-04 12:58:03 +0100

    render and fetch small gards with id, name, type an picture

--- script.js
+++ script.js
@@ -1,43 +1,56 @@
-function init(){
-    fetchData();
+let allPokemonData = [];
+let currentOffset = 0;
+const limit = 30;
+
+function init() {
+  fetchData();
 }
 
-async function fetchData(){
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon-species/pikachu');
-    const responseAsJson = await response.json(); 
-    console.log(responseAsJson);
-    
-    renderCards(responseAsJson);
+async function fetchData() {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${currentOffset}`);
+  const responseAsJson = await response.json();
+  const pkmnsList = responseAsJson.results;
+
+  for (let i = 0; i < pkmnsList.length; i++) {
+    const detailResponse = await fetch(pkmnsList[i].url);
+    const pkmnsDetailAsJson = await detailResponse.json();
+    allPokemonData.push(pkmnsDetailAsJson);
+  }
+
+  currentOffset += limit;
+  renderCards();
 }
 
-function renderCards(responseAsJson){
-    const cardsContainerRef = document.getElementById('cardsContainer');
-    // cardsContainerRef.innerHTML = '';
-    responseAsJson.forEach((arrayElement) => {
-            cardsContainerRef.innerHTML += getCardsHTML(arrayElement);
-        })
+async function renderCards() {
+  const cardsContainerRef = document.getElementById("cardsContainer");
+  cardsContainerRef.innerHTML = "";
+
+  for (let i = 0; i < allPokemonData.length; i++) {
+    const pokemon = allPokemonData[i];
+    console.log(pokemon);
+    cardsContainerRef.innerHTML += getCardsHTML(pokemon);
+  }
 }
 
-function getCardsHTML(arrayElement) {
-    return `<div class="card" id="card">
+
+function getCardsHTML(pokemon) {
+  const image = pokemon.sprites.other["official-artwork"].front_default;
+  
+  return `<div class="card" id="card">
           <div class="card-inner" id="cardInner">
             <div class="card-content">
               <div class="headline">
-                <h2>Pikachu</h2>
+                <h2>#${pokemon.id} ${pokemon.name}</h2>
               </div>
               <div class="img-container">
-                <img
-                  class="card-img"
-                  src="./assets/pikachu.png"
-                  alt="Pokemonname"
-                />
+                <img class="card-img" src="${image}" alt="${pokemon.name}"/>
               </div>
               <div class="card-info">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <p>Type: ${pokemon.types[0].type.name}</p>
               </div>
             </div>
             <div class="card-border">
-              <div class="border-row top">
+              <div class="border-row-top">
                 <div class="dot"></div>
                 <div class="rectangle horizontal"></div>
                 <div class="dot"></div>
@@ -50,7 +63,7 @@ function getCardsHTML(arrayElement) {
                 <div class="rectangle vertical"></div>
               </div>
 
-              <div class="border-row bottom">
+              <div class="border-row-botom">
                 <div class="dot"></div>
                 <div class="rectangle horizontal"></div>
                 <div class="dot"></div>
